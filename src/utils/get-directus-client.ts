eb822cbf13839740db7bb16f00e862e712ea6225
AllarKalina <allarkalina@gmail.com>
2023-03-24 04:26:46 +0200

    Add env var

--- src/utils/get-directus-client.ts
+++ src/utils/get-directus-client.ts
@@ -5,6 +5,8 @@ export const getDirectusClient = async () => {
 
   if (await directus.auth.token) return directus;
 
+  console.log("wtf");
+
   if (import.meta.env.DIRECTUS_EMAIL && import.meta.env.DIRECTUS_PASSWORD) {
     await directus.auth.login({
       email: import.meta.env.DIRECTUS_EMAIL,
