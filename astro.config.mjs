3eef331f6dc36cbd8aa736ed351cb3c095e36174
AllarKalina <allarkalina@gmail.com>
2023-03-28 20:42:54 +0300

    Polish for relase

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,11 +9,15 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 import image from "@astrojs/image";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://evus.vet",
   integrations: [
     react(),
     tailwind(),
+    sitemap(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
--- src/modules/HomePage/components/HeroImageItem.tsx
+++ src/modules/HomePage/components/HeroImageItem.tsx
@@ -3,25 +3,19 @@ import { getAssetURL } from "../../../utils/get-asset-url";
 
 interface Props {
   imageId: string;
-  parallaxModifier?: number;
   delay?: number;
 }
 
-const HeroImageItem: React.FC<Props> = ({
-  imageId,
-  delay,
-  parallaxModifier,
-}) => {
+const HeroImageItem: React.FC<Props> = ({ imageId, delay }) => {
   const { scrollY } = useScroll();
-  const y1 = useTransform(scrollY, [0, 600], [0, -35]);
+  const y1 = useTransform(scrollY, [0, 1000], [0, -35]);
 
   return (
     <motion.li
-      initial={{ y: 16, opacity: 0 }}
-      whileInView={{ y: 0, opacity: 1 }}
+      initial={{ y: y1.get() + 16, opacity: 0 }}
+      whileInView={{ y: y1.get(), opacity: 1 }}
       transition={{ ease: [0.74, 0.07, 0.38, 0.99], duration: 0.72, delay }}
       viewport={{ once: true }}
-      style={{ y: y1 }}
       className="h-[330px] w-[300px] overflow-hidden rounded-lg shadow-lg lg:w-[260px]"
     >
       <img
@@ -33,7 +27,7 @@ const HeroImageItem: React.FC<Props> = ({
             width: 300,
             height: 330,
             fit: "cover",
-            quality: 80,
+            quality: 90,
           }) as string
         }
       />
