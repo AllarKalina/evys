408bc1ffd46698e89ea104d204bbe47e85a8d85a
AllarKalina <allarkalina@gmail.com>
2023-05-11 04:02:11 +0300

    Create featured blog section

--- src/components/UI/Animated/AnimatedBlock.tsx
+++ src/components/UI/Animated/AnimatedBlock.tsx
@@ -2,15 +2,17 @@ import { motion } from "framer-motion";
 
 interface Props {
   children: React.ReactNode;
+  className?: string;
   delay?: number;
 }
-const AnimatedBlock: React.FC<Props> = ({ children, delay }) => {
+const AnimatedBlock: React.FC<Props> = ({ children, delay, className }) => {
   return (
     <motion.div
       initial={{ y: 24, opacity: 0 }}
       whileInView={{ y: 0, opacity: 1 }}
       transition={{ ease: [0.74, 0.07, 0.38, 0.99], duration: 0.72, delay }}
       viewport={{ once: true }}
+      className={className}
     >
       {children}
     </motion.div>
